ed2d0cb766ca90747ef66da78bdb3359cd12a031
arojmar <arojmar@gmail.com>
2018-11-02 03:35:24 +0100

    Updated the methods for Case 2

---
+++ src/test/java/stepDefinitions/SupportPageLinksDefinitions.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import serenitySteps.SearchButtonSteps;
+
+public class SupportPageLinksDefinitions {
+
+
+    @Given("^the support page is correctly displayed$")
+    public void isSupportPageDisplayedCorrectly(){
+
+    }
+
+}
+
